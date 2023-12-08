5f61f6511625620d3183b713d94203df42011e5c
Edson Litsur <johnlitsur@gmail.com>
2023-12-08 16:05:18 +0200

    creating error handler

--- api/index.js
+++ api/index.js
@@ -35,13 +35,14 @@ app.use('/api/auth', authRouter);
 
 // Midllerware
 
-// app.use((err, req, res, next) => {
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || 'Internal Server Error';
-//     return res.status(statusCode).json({
-//         success: false,
-//         statusCode, 
-//         message,
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode, 
+        message,
 
-//     });
-//     });
+    });
+    });
+   
