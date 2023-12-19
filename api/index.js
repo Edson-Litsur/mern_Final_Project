5188e2bab4bf4c87cba64c2fb46ac74564b2ce93
Edson Litsur <johnlitsur@gmail.com>
2023-12-19 09:34:58 +0200

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 // MONGODB CONNECTION
@@ -20,7 +21,9 @@ mongoose
 const app = express();
 
 // Allowing JSON
-app.use(express.json())
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
    console.log('Server is run on port 3000!!');
